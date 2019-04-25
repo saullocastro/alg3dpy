a3ba7322d459888c8963033910f91263563c00ad
saullocastro <castrosaullo@gmail.com>
2019-04-25 11:24:16 +0200

    MAINT: improved distribution script

--- distribuite.bat
+++ distribuite.bat
@@ -1,4 +1,7 @@
-python3 setup.py sdist bdist_wheel
+rmdir /s /q dist
+rmdir /s /q build
+rmdir /s /q alg3dpy.egg-info
+python setup.py sdist bdist_wheel
 twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 twine upload dist/*
 
