23f3bf9c064b273de1634aa1ac6ebb740b0717f6
saullocastro <saullogiovani@gmail.com>
2021-07-31 01:23:34 +0200

    TST: more tests

--- alg3dpy/tests/test_angles.py
+++ alg3dpy/tests/test_angles.py
@@ -1,15 +1,32 @@
+from numpy import pi, cos, isclose
+
 from alg3dpy.angles import (cos2vecs, sin2vecs, sinplanevec, cosplanevec,
         angle2vecs, angle2planes, angleplanevec)
 from alg3dpy.constants import PLANEXY, PLANEXZ, X, Y, Z
 
-import numpy as np
-
 def test_angles():
-    assert angle2vecs(X, Y) == np.pi/2
-    assert sin2vecs([2, 0, 0], [0, 1, 0]) == 1.
-    assert cos2vecs([2, 0, 0], [0, 1, 0]) == 0.
-    assert sinplanevec(PLANEXY, Z) == 1.
-    assert cosplanevec(PLANEXY, Z) == 0.
-    assert angle2planes(PLANEXY, PLANEXZ) == np.pi/2
-    assert angleplanevec(PLANEXY, X) == 0
-    assert angleplanevec(PLANEXY, Z) == np.pi/2
+    assert isclose(angle2vecs(X, Y), pi/2)
+    assert isclose(sin2vecs([2, 0, 0], [0, 1, 0]), 1.)
+    assert isclose(cos2vecs([2, 0, 0], [0, 1, 0]), 0.)
+    assert isclose(sinplanevec(PLANEXY, Z), 1.)
+    assert isclose(cosplanevec(PLANEXY, Z), 0.)
+    assert isclose(angle2planes(PLANEXY, PLANEXZ), pi/2)
+    assert isclose(angleplanevec(PLANEXY, X), 0)
+
+    assert isclose(angleplanevec(PLANEXY, Z), pi/2)
+    assert isclose(cosplanevec(PLANEXY, Z), cos(pi/2))
+
+    assert isclose(angleplanevec(PLANEXY, -Z), -pi/2)
+    assert isclose(cosplanevec(PLANEXY, -Z), cos(-pi/2))
+
+    assert isclose(angleplanevec(PLANEXY, [1, 0, 1]), pi/4)
+    assert isclose(cosplanevec(PLANEXY, [1, 0, 1]), cos(pi/4))
+
+    assert isclose(angleplanevec(PLANEXY, [-1, 0, +1]), pi/4)
+    assert isclose(cosplanevec(PLANEXY, [-1, 0, +1]), cos(pi/4))
+
+    assert isclose(angleplanevec(PLANEXY, [-1, 0, -1]), -pi/4)
+    assert isclose(cosplanevec(PLANEXY, [-1, 0, -1]), cos(-pi/4))
+
+    assert isclose(angleplanevec(PLANEXY, [+1, 0, -1]), -pi/4)
+    assert isclose(cosplanevec(PLANEXY, [+1, 0, -1]), cos(-pi/4))
